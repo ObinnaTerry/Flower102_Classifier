5b4943e7ede2859a4a46eb08e67727bd1e60696f
youngjay90 <3254048915@qq.com>
2020-02-05 10:19:31 +0200

    feat: Add method for proceesing image to be used in prediction

--- predict_utils.py
+++ predict_utils.py
@@ -22,3 +22,19 @@ class PredictUtils:
 
     return model
 
+    def process_image(image):
+
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    image = Image.open(image)
+
+    image_norm = transforms.Compose([transforms.Resize(255),
+                                    transforms.CenterCrop(244),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])])
+    
+    return image_norm(image)
+
+    
