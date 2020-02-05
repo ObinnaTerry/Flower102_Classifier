a47c68169a491d2659763ec3db10baa5f4591cd4
youngjay90 <3254048915@qq.com>
2020-02-05 10:18:21 +0200

    feat: Add method for loading model

--- predict_utils.py
+++ predict_utils.py
@@ -6,3 +6,19 @@ class PredictUtils:
 
 		pass
 
+	def load_checkpoint(filepath):
+    
+    checkpoint = torch.load(filepath, map_location=lambda storage, loc:storage)
+
+    model = checkpoint['model']
+    model.classifier = checkpoint['classifier']
+    model.class_to_idx = checkpoint['class_to_idx']
+    model.load_state_dict(checkpoint['state_dict'])
+    
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+        
+    model.eval()
+
+    return model
+
