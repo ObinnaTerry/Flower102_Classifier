1b0af6573a27d383e2573f84c451862a104af968
youngjay90 <3254048915@qq.com>
2020-02-07 15:03:31 +0200

    refactor: Comment out no longer used map_to_name arg

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ my_parser = argparse.ArgumentParser(description='Script used making predictions.
 my_parser.add_argument('--image_path', action='store', dest='image_path', type=str, required=True)
 my_parser.add_argument('--model_path', action='store', dest='model_path', type=str, required=True)
 my_parser.add_argument('--top_k', action='store', dest='top_k', type=int, default=3)
-my_parser.add_argument('--map_to_name', action='store', dest='map_to_name', type=bool, default=True)
+# my_parser.add_argument('--map_to_name', action='store', dest='map_to_name', type=bool, default=True)
 my_parser.add_argument('--mode', action='store', dest='mode', type=str, default='cpu')
 my_parser.add_argument('--json_file', action='store', dest='json_file', type=str, default=None)
 
